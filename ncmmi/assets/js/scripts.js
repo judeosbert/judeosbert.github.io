1b78eb23671322c568bcb824081b8c61ed8e2c80
judeosbert <judeosby@gmail.com>
2016-04-04 13:27:20 +0530

    Website for NCMMI

---
+++ ncmmi/assets/js/scripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Jude Osbert K on 22-03-2016.
+ */
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 20,
+        time: 1000
+    });
+});
---
+++ ncmmi/index.php
@@ -0,0 +1,162 @@
+<?php
+?>
+
+<!Doctype html>
+    <html>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/site.css" rel="stylesheet" />
+        <link href="assets/css/animate.css" rel="stylesheet" />
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/counterup.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        <script src="assets/js/commentslider.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+<head>
+
+</head>
+<body>
+
+<div class="container-fluid firstframe_container">
+
+    <div class="row">
+        <div class="mainheader">
+        <div class="col-lg-1 col-sm-12">
+
+            <img src="assets/images/Vellore_Institute_of_Technology_seal.svg.png" class="navbar_img center-block " />
+        </div>
+            <div class="col-lg-10 text-lg-center col-sm-12 text-sm-center col-md-12 text-md-center navbardiv center-block col-xs-12 text-xs-center ">
+                <ul class="list-inline center-block">
+                    <li class="list-inline-item">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">Speakers</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">Workshops</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">Papers</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">NCMMI'15</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-1"></div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-lg-12 pusher70">
+
+        </div>
+        <div class="col-lg-12 text-lg-center ">
+            <p><img src="assets/images/f524cf16-8436-4864-8f4c-612e55492ee2.png" id="eventlogo" class="img-fluid center-block "/></p>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <ul class="list-inline content-group">
+                <li class="list-inline-item">
+
+                </li>
+                <li class="list-inline-item">
+
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 pusher70"></div>
+        <div class="col-lg-12 text-lg-center col-sm-12 text-sm-center col-md-12 text-md-center center-block col-xs-12 text-xs-center">
+            <ul class="list-inline col-md-12 text-md-center">
+                <li class="list-inline-item tex"><button class="btn registerbtn">Register</button></li>
+
+                <li class="list-inline-item"><button class="btn brochurebtn">Brochure</button></li>
+            </ul>
+            <div class="col-lg-12 pusher70">
+                <br /><br /><br /><br />
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="container-fluid summary_conatiner ">
+    <div class="row" >
+
+        <br />
+      <div class="col-lg-12 text-lg-center col-sm-12 text-sm-center">
+
+          <p id="ncmmi2015">Ncmmi 2015</p>
+      </div>
+        <div class="col-lg-12 text-lg-center col-sm-12 text-sm-center" >
+
+        <ul class="list-inline summary_list" >
+
+            <li class="list-inline-item">
+                <p><span class="number counter">125</span><br /><span class="description">Participants</span></p>
+            </li>
+            <li class="list-inline-item">
+                <p><span class="number counter">20</span><br /><span class="description">Papers Presented</span></p>
+            </li>
+            <li class="list-inline-item">
+                <p><span class="number counter">15</span><br /><span class="description">Something else</span></p>
+            </li>
+            <li class="list-inline-item">
+                <p><span class="number counter">15</span><br /><span class="description">Something else</span></p>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
+<div class="container-fluid reviews_container">
+    <div class="row">
+
+        <div class="col-lg-12 text-lg-center">
+            <div id="slider">
+
+                <ul>
+                    <li><div class="reviews text-lg-center">
+                            
+                            <blockquote class="center-block">
+                                <p id="reviewcontent" style="justify-content: center;">For 50 years, WWF has been protecting the future of nature.</p>
+                                <p id="reviwer"> <span id="reviwername">G Viswanathan</span><br />Vice Chancellor, VIT University </p>
+                            </blockquote>
+                        </div> </li>
+                    <li><div class="reviews text-lg-center">
+                            <blockquote>
+                                <p id="reviewcontent">Someone said something about this event.</p>
+                                <p id="reviwer"> <span id="reviwername">ABCD</span><br />XYZ<br />ghnh </p>
+                            </blockquote>
+                        </div> </li>
+                    <li><div class="reviews text-lg-center">
+                            <blockquote>
+                                <p id="reviewcontent">For 50 years, WWF has been protecting the future of nature.</p>
+                                <p id="reviwer"> <span id="reviwername">asdasdan</span><br />Vsadasellor<br />Vsadsaersity </p>
+                            </blockquote>
+                        </div> </li>
+                    <li><div class="reviews text-lg-center">
+
+                            <blockquote>
+                                <p id="reviewcontent">Someone said something about this event.</p>
+                                <p id="reviwer"> <span id="reviwername">ABCD</span><br />XYZ<br />ghnh </p>
+                            </blockquote>
+                        </div> </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
