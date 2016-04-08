046abdeaeb32e50915962ae2f6929afa084b038e
judeosbert <judeosby@gmail.com>
2016-04-08 14:44:49 +0530

    About us
    Try 1 About US

--- ncmmi/assets/js/scripts.js
+++ ncmmi/assets/js/scripts.js
@@ -1,9 +1,13 @@
 /**
  * Created by Jude Osbert K on 22-03-2016.
  */
-jQuery(document).ready(function($) {
+$(document).ready(function() {
+
+
     $('.counter').counterUp({
         delay: 20,
         time: 1000
     });
+
+
 });
